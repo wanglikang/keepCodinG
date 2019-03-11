e5247a3f0a2681ce6e7aa6ecefe2b6237e58f558
王利康 <wanglikang@users.noreply.github.com>
2019-03-11 17:35:21 +0800

    Create Solution009.java

---
+++ src/cn/wlk/keepcoding/leetcode/Solution009.java
@@ -0,0 +1,30 @@
+package cn.wlk.keepcoding.leetcode;
+
+import java.util.Stack;
+
+public class Solution009 {
+	public static void main(String[] args) {
+		Solution009 s = new Solution009();
+		System.out.println(s.isPalindrome(234322));
+
+	}
+	public boolean isPalindrome(int x) {
+		if(x<0)
+			return false;
+		Stack<Integer> stack1 = new Stack<>();
+
+		int xx = x;
+		while(x!=0){
+			stack1.push(x%10);
+			x = x/10;
+		}
+		boolean result = true;
+		while(!stack1.empty()){
+			if(stack1.pop()!=xx%10){
+				result = false;
+			}
+			xx = xx/10;
+		}
+		return result;
+	}
+}
