b9bb1d458a23e10a0feff509f3749ea23f9b2ef3
WLK-acer <1024196018@qq.com>
2020-02-15 22:33:58 +0800

    18. 四数之和：先排序，设两个指针，然后遍历

--- src/cn/wlk/keepcoding/leetcode/n018.java
+++ src/cn/wlk/keepcoding/leetcode/n018.java
@@ -2,6 +2,8 @@ package cn.wlk.keepcoding.leetcode;
 
 import org.junit.Test;
 
+import java.util.*;
+
 /**
  * 18. 四数之和
  *
@@ -27,7 +29,60 @@ public class n018 {
 
     @Test
     public void test() {
+        n018 t = new n018();
+//        int[] in = new int[]{1, 0, -1, 0, -2, 2};
+        int[] in = new int[]{0,0,0,0};
+        List<List<Integer>> result = t.fourSum(in, 0);
+        result.stream().forEach(v->{
+            v.stream().forEach(vv-> System.out.print(vv+" "));
+            System.out.println();
+        });
+    }
 
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        int n = nums.length;
+        if(n<4){
+            return result;
+        }
+        int a,b,c,d;
+        for(a = 0;a<=n-4;a++){
+            if(a>0 && nums[a]==nums[a-1] ) {
+                //确保没有重复的结果元素
+                continue;
+            }
+            for(b = a+1;b<=n-3;b++){
+                if(b>a+1 && nums[b]==nums[b-1]){
+                    continue;
+                }
+                c = b+1;
+                d = n-1;
+                while(c<d){
+
+                    if(nums[a]+nums[b]+nums[c]+nums[d]<target){
+                        c++;
+                    }else if(nums[a]+nums[b]+nums[c]+nums[d]>target){
+                        d--;
+                    }else {
+                        result.add(new ArrayList<>(Arrays.asList(nums[a],nums[b],nums[c],nums[d])));
+                        while(c<d && nums[c+1]==nums[c]){
+                            c++;
+                        }
+                        while(c<d && nums[d-1]==nums[d]){
+                            d--;
+                        }
+
+                        c++;
+                        d--;
+                    }
+//                    System.out.println(c+"-"+d);
+                }
+            }
+        }
+
+        return result;
     }
 
 }
