561da456e42b12746caa8d3bb3b0f1eebaf8c47a
nx <wlk265683@cainiao.com>
2024-03-30 18:23:11 +0800

    移动源代码的位置，并删除无用代码，使适配idea的规范

--- src/main/java/cn.wlk.keepcoding.main.old/toutiao1.java
+++ src/main/java/cn.wlk.keepcoding.main.old/toutiao1.java
@@ -1,3 +1,5 @@
+package cn.wlk.keepcoding.main.old;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
--- src/main/java/cn.wlk.keepcoding.main.old/toutiao3.java
+++ src/main/java/cn.wlk.keepcoding.main.old/toutiao3.java
@@ -1,3 +1,5 @@
+package cn.wlk.keepcoding.main.old;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
